6a3f379637ca6f5871a7487ce5f396f54013f058
robert7277 <brunner.robert@protonmail.com>
2021-12-08 16:57:19 -0500

    MORE STUFFFFFF

--- database.php
+++ database.php
@@ -19,7 +19,7 @@ function display_restaurant_list($db){
     while ($entry = $restaurants->fetch()){
         ?>
         <div>
-            <a href="detail.php?id="<?=$entry['RID']?>><?=$entry['name']?></a>
+            <a href="detail.php?id=<?=$entry['RID']?>"><?=$entry['name']?></a>
             <h4><?=$entry['category']?></h4>
         </div>
 <?php
@@ -28,7 +28,7 @@ function display_restaurant_list($db){
 
 function display_menu($db, $id){
     $menu = $db->query('select name, description, price from menu_items where RID = ' . $id);
-    while($item = $menu ->fetch()){
+    while($item = $menu->fetch()){
         ?>
             <div>
                 <h3><?=$item['name']?></h3>
@@ -43,12 +43,22 @@ function display_restaurant_detail($db, $id){
     $restaurant = $db->query('select name, category, AID from restaurants where RID = ' . $id);
     $r_temp = $restaurant->fetch();
     $r_address = $db->query('select street, city, state, zip, phone from address where AID = '.$r_temp['AID']);
-    $temp[] = $r_address->fetch()
+    $a_temp[] = $r_address->fetch();
+    print_r($a_temp);
+    print_r($r_temp);
     ?>
     <div>
         <h1><?=$r_temp['name']?></h1>
         <h2><?=$r_temp['category']?></h2>
-        <p><?=$temp['street']. ', '.$temp['city'].' '.$temp['state']. ', '.$temp['zip'].'. Phone Number= '.$temp['phone']?></p>
+        <p><?=$a_temp[0]['street'], ', ',$a_temp[0]['city'],' ',$a_temp[0]['state'], ', ',$a_temp[0]['zip'],'. Phone Number= ',$a_temp[0]['phone']?></p>
     </div>
 <?php
+}
+
+function create_user($user_array, $db){
+    $db->query($db->query("INSERT INTO address(AID, street, city, state, zip, phone) VALUES (NULL, '".$user_array['street']."', '".$user_array['city']."', '".$user_array['state']."','".$user_array['zip']."', '".$user_array['phone']."'"));
+    $AID = $db->lastInsertId();
+    $db->query("insert into auth(pid, password) values(null, '".$user_array['password']."')");
+    $PID = $db->lastInsertID();
+    $db->query("insert into users(uid, aid, pid, name, email, isauth) values(null, '".$AID."','".$PID."', '".$user_array['name']."', '".$user_array['email']."', null)");
 }
